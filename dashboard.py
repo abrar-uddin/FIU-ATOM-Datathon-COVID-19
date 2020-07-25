cd12d7b950a07d04c6d431518c12c527e21b8a8c
Ahmed Uddin <ahmeduddinict@gmail.com>
2020-07-25 17:46:07 -0400

    working table

--- dashboard.py
+++ dashboard.py
@@ -1,2 +1,27 @@
 import streamlit as st
 import pandas as pd
+import requests
+import json
+
+import json
+
+st.title('Local COVID-19 Tracker')
+
+url = 'https://services1.arcgis.com/CY1LXxl9zlJeBuRZ/arcgis/rest/services/Florida_COVID19_Case_Line_Data_NEW/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+
+try:
+    status = requests.get(url)
+except:
+    st.write('## **API DOWN!**')
+
+query = requests.get(url)
+query = query.json()['features']
+query = pd.json_normalize(query)
+st.write(query)
+
+
+
+
+
+
+
