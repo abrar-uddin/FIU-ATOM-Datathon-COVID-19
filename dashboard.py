60a19e58deeef07e873e84762d13cd73a117be5c
Ahmed Uddin <ahmeduddinict@gmail.com>
2020-07-25 18:46:04 -0400

    update api

--- dashboard.py
+++ dashboard.py
@@ -7,17 +7,30 @@ import json
 
 st.title('Local COVID-19 Tracker')
 
-url = 'https://services1.arcgis.com/CY1LXxl9zlJeBuRZ/arcgis/rest/services/Florida_COVID19_Case_Line_Data_NEW/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+url = 'https://services1.arcgis.com/CY1LXxl9zlJeBuRZ/arcgis/rest/services/Florida_COVID19_Cases/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
 
 try:
     status = requests.get(url)
+    raw_data = requests.get(url)
 except:
     st.write('## **API DOWN!**')
 
-query = requests.get(url)
-query = query.json()['features']
-query = pd.json_normalize(query)
-st.write(query)
+
+raw_data = raw_data.json()['features']
+raw_data = pd.json_normalize(raw_data)
+raw_data = raw_data.drop(['attributes.OBJECTID', 'attributes.OBJECTID_12_13', 'attributes.State',
+                          'attributes.County_1','attributes.DEPCODE', 'attributes.COUNTY',
+                          'geometry.rings', 'attributes.Shape__Length', 'attributes.Shape__Area'], axis=1)
+
+
+broward = raw_data['attributes.COUNTYNAME'] == 'BROWARD'
+dade = raw_data['attributes.COUNTYNAME'] == 'DADE'
+collier = raw_data['attributes.COUNTYNAME'] == 'COLLIER'
+local_counties = raw_data[broward]
+local_counties = local_counties.append(raw_data[dade])
+local_counties = local_counties.append(raw_data[collier])
+st.write(local_counties)
+
 
 
 
